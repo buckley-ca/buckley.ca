dbb2c9dfbd7e41b6269648f21957139028aef7bc
Sean Buckley <seanbuckley@users.noreply.github.com>
2021-10-13 11:27:37 -0700

    Add shadow to logo and text

--- src/Logo.svelte
+++ src/Logo.svelte
@@ -28,6 +28,8 @@
     max-height: 800px;
     min-width: 40px;
     min-height: 40px;
+    -webkit-filter: drop-shadow( 0.4px 0.8px 0.8px rgba(0, 0, 0, .233));
+    filter: drop-shadow( 0.4px 0.8px 0.8px rgba(0, 0, 0, 0.233));
   }
   .sblogo-svg-s,
   .sblogo-svg-b {
