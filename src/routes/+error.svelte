595fcb74173b4aacab6786f3cae980e6c0154f5c
Sean Buckley <seanbuckley@users.noreply.github.com>
2022-10-04 15:52:27 -0700

    Add error status info

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,15 +1,17 @@
 <script>
 	export const prerender = true;
+	import { page } from '$app/stores';
 </script>
 
+// @ts-nocheck
 <svelte:head>
 	<title>Buckley.ca - Error</title>
 	<meta name="description" content="Buckley.ca - Error" />
 </svelte:head>
 
 <section>
-	<h1>Error</h1>
-	<p>Uh oh, you've encoutered an error. Please go <a href="/">home</a>.</p>
+	<h1>Error - {$page.status}: {$page.error.message}</h1>
+	<p>Uh oh, you've encoutered an error. Please go back <a href="/">home</a>.</p>
 </section>
 
 <style>
