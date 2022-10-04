62b39cd7793e3e861aabc7a2ab924a46bd2bcd27
Sean Buckley <seanbuckley@users.noreply.github.com>
2022-10-04 15:43:22 -0700

    Add intial default error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script>
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>Buckley.ca - Error</title>
+	<meta name="description" content="Buckley.ca - Error" />
+</svelte:head>
+
+<section>
+	<h1>Error</h1>
+	<p>Uh oh, you've encoutered an error. Please go <a href="/">home</a>.</p>
+</section>
+
+<style>
+	section {
+		display: grid;
+		place-content: center;
+	}
+</style>
