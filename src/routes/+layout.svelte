e33c82120b058b7256f2c39cab3c3fb841ccd524
Sean Buckley <seanbuckley@users.noreply.github.com>
2023-01-05 10:51:27 -0800

    Remove browser specific background scaling hacks

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,8 +16,6 @@
 		-moz-background-size: cover;
 		-o-background-size: cover;
 		background-size: cover;
-		/* filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='../img/bg-small.jpg', sizingMethod='scale'); */
-		/* -ms-filter: "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='../img/bg-small.jpg', sizingMethod='scale')"; */
 		height: 100vh;
 	}
 </style>
