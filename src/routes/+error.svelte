30c7ccf124b8202124ef7903e5a84af0cb59215a
Sean Buckley <seanbuckley@users.noreply.github.com>
2023-03-17 14:34:29 -0700

    Improve Error page styling

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -8,14 +8,41 @@
 	<meta name="description" content="Buckley.ca - Error" />
 </svelte:head>
 
-<section>
-	<h1>Error - {$page.status}: {$page.error.message}</h1>
-	<p>Uh oh, you've encountered an error. Please go back <a href="/">home</a>.</p>
+<section class="center">
+	<div class="logo-container">
+		<h1>{$page.status} | {$page.error.message}</h1>
+		<p>Uh oh, you've encountered an error. Please go back <a href="/">home</a>.</p>
+	</div>
 </section>
 
 <style>
-	section {
-		display: grid;
-		place-content: center;
+	.center {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 95vh;
+	}
+
+	.logo-container {
+		text-align: center;
+		background-color: rgba(255, 255, 255, 0.75);
+		padding: 1rem;
+		-moz-border-radius: 10px;
+		-webkit-border-radius: 10px;
+		border-radius: 10px;
+	}
+
+	h1 {
+		font-size: 3rem;
+		font-weight: 600;
+		margin: 0;
+		text-shadow: 0.4px 0.8px 0.8px hsl(0deg 0% 0% / 0.15);
+	}
+
+	@media (min-width: 640px) {
+		section {
+			max-width: none;
+		}
 	}
 </style>
