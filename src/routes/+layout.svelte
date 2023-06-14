4dd99e1e194c39b0411d3f0a8ce6a0fbbcc7a408
Sean Buckley <seanbuckley@users.noreply.github.com>
2023-06-14 10:56:12 -0700

    Improve background image responsiveness

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,11 +10,22 @@
 
 <style>
 	.background {
-		background: url(https://res.cloudinary.com/buckey-ca/image/upload/v1634141535/background-1600_ovnjor.jpg)
+		background: url(https://res.cloudinary.com/buckey-ca/image/upload/f_auto,q_auto,dpr_auto/background-1600_ovnjor.jpg)
 			fixed no-repeat center center;
 		-webkit-background-size: cover;
 		-moz-background-size: cover;
 		-o-background-size: cover;
 		background-size: cover;
+	}
+
+	@media (max-width: 640px) {
+		.background {
+			background: url(https://res.cloudinary.com/buckey-ca/image/upload/f_auto,q_auto,c_fill,w_800,dpr_auto/background-1600_ovnjor.jpg)
+				fixed no-repeat center center;
+			-webkit-background-size: cover;
+			-moz-background-size: cover;
+			-o-background-size: cover;
+			background-size: cover;
+		}
 	}
 </style>
